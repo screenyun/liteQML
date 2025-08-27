d90d7640bfd7eeb12fad703336b520c3e27fccbb
screenyun <screenyun@gmail.com>
2025-08-27 22:07:18 +0800

    优化代码

--- demo/bundle.js
+++ demo/bundle.js
@@ -255,12 +255,361 @@ class CoreObject {
     }
 }
 
-class App extends CoreObject {
+class Anchors extends CoreObject {
     constructor(parent, params) {
         super(parent, params);
-        this._id['root']=this;
+        this.addProperty('centerIn');
+        this.addProperty('fill');
+        this.addProperty('verticalCenter');
+        this.addProperty('horizontalCenter');
+        this.centerInChanged.connect(this.onCenterInChanged.bind(this));
+        this.fillChanged.connect(this.onFillChanged.bind(this));
+        this.horizontalCenterChanged.connect(this.onHorizontalCenterChanged.bind(this));
+        this.verticalCenterChanged.connect(this.onVerticalCenterChanged.bind(this));
+        this.centerIn = params && params.centerIn? params.centerIn: null;
+        this.fill = params && params.fill? params.fill: null;
+        this.verticalCenter = params && params.verticalCenter? params.verticalCenter: 0;
+        this.horizontalCenter = params && params.horizontalCenter? params.horizontalCenter: 0;
+
+        this.registerAll();
+
+    }
+
+    onCenterInChanged() { 
+    {
+        this.parent.x = Binding((function () {return ((this.centerIn.width / 2) - (this.parent.width / 2));}).bind(this), this, ['centerIn.width', 'parent.width']);
+        this.parent.y = Binding((function () {return ((this.centerIn.height / 2) - (this.parent.height / 2));}).bind(this), this, ['centerIn.height', 'parent.height']);
+
+    }
+
+
+    }
+    onFillChanged() { 
+    {
+        this.parent.width = Binding((function () {return this.fill.width;}).bind(this), this, ['fill.width']);
+        this.parent.height = Binding((function () {return this.fill.height;}).bind(this), this, ['fill.height']);
+
+    }
+
+
+    }
+    onHorizontalCenterChanged() { 
+    this.parent.x = (this.horizontalCenter - (this.parent.width / 2));
+
+
+
+
+    }
+    onVerticalCenterChanged() { 
+    this.parent.y = (this.verticalCenter - (this.parent.height / 2));
+
+
+
+
+    }
+
+}
+
+class Item extends CoreObject {
+    constructor(parent, params) {
+        super(parent, params);
         this.addProperty('x');
-        this.x = params && params.x? params.x: 100;
+        this.addProperty('y');
+        this.addProperty('width');
+        this.addProperty('height');
+        this.addProperty('rotation');
+        this.addProperty('clip');
+        this.addProperty('visible');
+        this.addProperty('anchors');
+        this.addProperty('verticalCenter');
+        this.addProperty('horizontalCenter');
+        this.x = params && params.x? params.x: 0;
+        this.y = params && params.y? params.y: 0;
+        this.width = params && params.width? params.width: 0;
+        this.height = params && params.height? params.height: 0;
+        this.rotation = params && params.rotation? params.rotation: 0;
+        this.clip = params && params.clip? params.clip: false;
+        this.visible = params && params.visible? params.visible: true;
+
+        class Item_anchors extends Anchors {
+            constructor(parent, params) {
+                super(parent, params);
+
+                this.registerAll();
+
+            }
+
+
+        }
+        this.anchors = params && params.anchors? params.anchors: new Item_anchors(this);
+        this.verticalCenter = params && params.verticalCenter? params.verticalCenter: Binding(() => { return (this.height / 2); }, this, ["height"], []);
+        this.horizontalCenter = params && params.horizontalCenter? params.horizontalCenter: Binding(() => { return (this.width / 2); }, this, ["width"], []);
+
+        this.registerAll();
+
+    }
+    draw(layer) {
+    if (this.visible) this.drawImpl(layer); 
+
+
+    }
+    drawImpl(layer) {
+    {
+        let scene=layer.scene, ctx=scene.context
+        this.beginNode(layer);
+        this.drawChildren(layer);
+        this.endNode(layer);
+
+    }
+    }
+    beginNode(layer) {
+    {
+        let ctxScene=layer.scene.context
+        let ctxHit=layer.hit.context
+        ctxScene.save();
+        ctxHit.save();
+        ctxScene.translate((this.x + (this.width / 2)), (this.y + (this.height / 2)));
+        ctxScene.rotate(((this.rotation * 3.14159) / 180));
+        ctxHit.translate((this.x + (this.width / 2)), (this.y + (this.height / 2)));
+        ctxHit.rotate(((this.rotation * 3.14159) / 180));
+        if (this.clip) {
+            this.outline(ctxScene);
+            ctxScene.clip();
+            this.outline(ctxHit);
+            ctxHit.clip();
+
+        } 
+
+    }
+    }
+    endNode(layer) {
+    {
+        let ctxScene=layer.scene.context
+        let ctxHit=layer.hit.context
+        ctxScene.restore();
+        ctxHit.restore();
+
+    }
+    }
+    outline(ctx) {
+    {
+        ctx.beginPath();
+        ctx.rect((-this.width / 2), (-this.height / 2), this.width, this.height);
+
+    }
+    }
+    drawChildren(layer) {
+    {
+        let ctxScene=layer.scene.context
+        let ctxHit=layer.hit.context
+        ctxScene.save();
+        ctxHit.save();
+        ctxScene.translate((-this.width / 2), (-this.height / 2));
+        ctxHit.translate((-this.width / 2), (-this.height / 2));
+        for(let i=0; (i < this.children.length); i++)
+            if ((this.children[i] instanceof Item)) this.children[i].draw(layer); 
+
+
+        ctxScene.restore();
+        ctxHit.restore();
+
+    }
+    }
+
+
+}
+
+class Image extends Item {
+    constructor(parent, params) {
+        super(parent, params);
+        this.addProperty('source');
+        this.sourceChanged.connect(this.onSourceChanged.bind(this));
+        this.source = params && params.source? params.source: '';
+
+        this.registerAll();
+
+    }
+    drawImpl(layer) {
+    {
+        let scene=layer.scene, ctx=scene.context
+        this.beginNode(layer);
+        if (this._imgElement) ctx.drawImage(this._imgElement, (-this.width / 2), (-this.height / 2)); 
+        this.drawChildren(layer);
+        this.endNode(layer);
+
+    }
+    }
+
+    onSourceChanged() { 
+    {
+        if (!this._imgElement) this._imgElement = globalThis.document.createElement('img'); 
+        this._imgElement.src = this.source;
+        this._imgElement.onload = (function () {{
+            this.width = this._imgElement.naturalWidth;
+            this.height = this._imgElement.naturalHeight;
+
+        }}).bind(this);
+
+    }
+
+
+    }
+
+}
+
+class Text extends Item {
+    constructor(parent, params) {
+        super(parent, params);
+        this.addProperty('text');
+        this.addProperty('color');
+        this.text = params && params.text? params.text: '';
+        this.color = params && params.color? params.color: "white";
+
+        this.registerAll();
+
+    }
+    drawImpl(layer) {
+    {
+        let scene=layer.scene, ctx=scene.context
+        let txt=ctx.measureText(this.text)
+        this.width = txt.width;
+        this.height = (txt.actualBoundingBoxAscent + txt.actualBoundingBoxDescent);
+        this.beginNode(layer);
+        ctx.fillStyle = this.color;
+        ctx.fillText(this.text, (-this.width / 2), (this.height / 2));
+        this.drawChildren(layer);
+        this.endNode(layer);
+
+    }
+    }
+
+
+}
+
+class Rectangle extends Item {
+    constructor(parent, params) {
+        super(parent, params);
+        this.addProperty('radius');
+        this.addProperty('color');
+        this.radius = params && params.radius? params.radius: 0;
+        this.color = params && params.color? params.color: 'white';
+
+        this.registerAll();
+
+    }
+    drawImpl(layer) {
+    {
+        let scene=layer.scene, ctx=scene.context
+        this.beginNode(layer);
+        this.roundRect(ctx, (-this.width / 2), (-this.height / 2), this.width, this.height, this.radius);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        this.drawChildren(layer);
+        this.endNode(layer);
+
+    }
+    }
+    outline(ctx) {
+    this.roundRect(ctx, (-this.width / 2), (-this.height / 2), this.width, this.height, this.radius);
+
+
+    }
+    roundRect(ctx,x,y,w,h,r) {
+    {
+        if ((w < (2 * r))) r = (w / 2); 
+        if ((h < (2 * r))) r = (h / 2); 
+        ctx.beginPath();
+        ctx.moveTo((x + r), y);
+        ctx.arcTo((x + w), y, (x + w), (y + h), r);
+        ctx.arcTo((x + w), (y + h), x, (y + h), r);
+        ctx.arcTo(x, (y + h), x, y, r);
+        ctx.arcTo(x, y, (x + w), y, r);
+        ctx.closePath();
+        return ctx;
+
+
+    }
+    }
+
+
+}
+
+class App extends Item {
+    constructor(parent, params) {
+        super(parent, params);
+        this.addSignal('positionChanged');
+        this.addSignal('clicked');
+        this.width = 500;
+        this.height = 500;
+
+        class App_Image_0 extends Image {
+            constructor(parent, params) {
+                super(parent, params);
+                this._id['ball']=this;
+                this.x = 100;
+                this.y = 100;
+                this.width = 200;
+                this.height = 200;
+                this.source = 'https://apng.onevcat.com/assets/elephant.gif';
+
+                this.registerAll();
+
+            }
+
+
+        }
+        this.appendChild(new App_Image_0(this));
+
+        class App_Text_1 extends Text {
+            constructor(parent, params) {
+                super(parent, params);
+                this._id['helloText']=this;
+                this.text = 'Hello world!';
+                this.color = 'balck';
+                this.anchors.verticalCenter = Binding(() => { return this.parent.verticalCenter; }, this, ["parent.verticalCenter"], []);
+                this.anchors.horizontalCenter = Binding(() => { return this.parent.horizontalCenter; }, this, ["parent.horizontalCenter"], []);
+
+                this.registerAll();
+
+            }
+
+
+        }
+        this.appendChild(new App_Text_1(this));
+
+        class App_Rectangle_2 extends Rectangle {
+            constructor(parent, params) {
+                super(parent, params);
+                this.addProperty('moved');
+                this.moved = params && params.moved? params.moved: false;
+                this.x = Binding(() => { return this.moved?190:10; }, this, ["moved"], []);
+                this.y = 50;
+                this.width = 200;
+                this.height = 200;
+                this.color = 'red';
+
+                this.registerAll();
+
+            }
+            mouseMoved() {
+            {
+                this.positionChanged.emit();
+                this.moved = !this.moved;
+                console.log('mouseMoved');
+
+            }
+            }
+            mouseClicked() {
+            {
+                this.clicked.emit();
+                console.log('mouseClicked');
+
+            }
+            }
+
+
+        }
+        this.appendChild(new App_Rectangle_2(this));
 
         this.registerAll();
 
@@ -270,6 +619,9 @@ class App extends CoreObject {
 }
 
 polyfill().then(() => {
+    if(!('window' in globalThis)) {
+        globalThis.window = {};
+    }
     window.qml = {};
     window.qml.rootObject = new App();
 });
